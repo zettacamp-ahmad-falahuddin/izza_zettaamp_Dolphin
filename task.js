fbaf9936ff6dbcc0c3f267dcef62af221c53fb06
zettacamp-ahmad-falahuddin <ahmad.falahuddin@zettacamp.pro>
2023-09-13 15:57:48 +0700

    jsAdvanced_day4

--- task.js
+++ task.js
@@ -14,7 +14,7 @@ const buku = [
   },
 ];
 
-async function purchase(purchased, book, stock, discountPercentage, taxPercentage, termMonths, additionalPrice = 1000) {
+async function purchase(book, purchased, stock, discountPercentage, taxPercentage, termMonths, additionalPrice = 1000) {
   const bookName = book.name;
   const bookPrc = book.price;
 
@@ -23,11 +23,23 @@ async function purchase(purchased, book, stock, discountPercentage, taxPercentag
 
   const term = [];
 
-  for (let i = 0; i < termMonths; i++) {
+  const currentDate = new Date();
+
+  for (let i = 1; i <= termMonths; i++) {
+    const nextMonthDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + i, currentDate.getDate());
     term.push({
-      date: i + 1 > 1 ? `on ${i + 1} next ${i + 1} months` : `on ${i + 1} next ${i + 1} month`,
+      date: nextMonthDate.toDateString(),
       payment: purchased < stock ? parseInt((1 / termMonths) * purchased * prcAftTax) : parseInt((1 / termMonths) * stock * prcAftTax),
     });
+  }
+
+  const payments = term.map((e) => e.payment);
+  const sum = payments.reduce((accumulator, currentValue) => accumulator + currentValue);
+  const totalPrice = parseInt(purchased < stock ? purchased * prcAftTax : stock * prcAftTax);
+
+  if (sum !== totalPrice) {
+    const diff = totalPrice - sum;
+    term[1].payment += diff;
   }
 
   [month1, ...otherMonths] = term;
@@ -41,13 +53,11 @@ async function purchase(purchased, book, stock, discountPercentage, taxPercentag
     console.log('out of stock, cannot be purchased again');
     console.log(`the book ${bookName} price is, ${parseInt(copyOfInitialStock * prcAftTax)}`);
   } else {
-    console.log(`the book ${bookName} price is', parseInt(purchased * prcAftTax)`);
+    console.log(`the book ${bookName} price is', ${parseInt(purchased * prcAftTax)}`);
     console.log('can be purchased again');
   }
 
   console.log('stock now:', stock);
-
-  console.log('due to policy we add additional price when using credit, please wait for the calculations...');
 
   for (let i = 0; i < termMonths; i++) {
     term[i].addtitionalPrice = additionalPrice;
@@ -105,17 +115,32 @@ app.get('/', (_, res) => {
 });
 
 app.post('/', async (req, res) => {
-  const purchased = req.body.purchased;
   const book = req.body.book;
+  const purchased = req.body.purchased;
   const stock = req.body.stock;
   const discountPercentage = req.body.discountPercentage;
   const taxPercentage = req.body.taxPercentage;
   const termMonths = req.body.termMonths;
   const additionalPrice = req.body.additionalPrice;
+  const dateToPay = req.body.dateToPay;
 
-  const hasil = await purchase(purchased, book[0], stock, discountPercentage, taxPercentage, termMonths, additionalPrice);
+  const hasil = await purchase(book[0], purchased, stock, discountPercentage, taxPercentage, termMonths, additionalPrice);
 
-  res.json(hasil);
+  const list_term_amount = [...new Set(hasil.map((e) => e.payment))];
+
+  const list_terms = new Map();
+  hasil.map((e) => list_terms.set(e.date, e));
+
+  const date_to_pay = new Date(dateToPay.year, dateToPay.month - 1, dateToPay.date);
+  const term_to_pay = list_terms.get(date_to_pay.toDateString());
+
+  const respondObject = {
+    list_term_amount: list_term_amount,
+    list_terms: Array.from(list_terms.entries()),
+    term_to_pay: term_to_pay,
+  };
+
+  res.json(respondObject);
 });
 
 app.get('/endpoint1', async (req, res) => {
